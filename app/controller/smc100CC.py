05d37a1d2e6532f535cbd26e66584668ce0c10b2
k4y0W <wojenskipawel945@gmail.com>
2024-10-25 23:27:33 +0200

    smc

--- app/controller/smc100CC.py
+++ app/controller/smc100CC.py
@@ -1,6 +1,28 @@
+import serial
+import time
+from serial import SerialException
+
+class smc100:
+    def __init__(self, port):
+        self.serial=serial.Serial()
+        self.serial.port = port 
+        self.serial.baudrate = 57600
+        self.serial.bytesize = serial.EIGHTBITS
+
+        try:
+            self.serial.open()
+            print("pradzi")
+        except SerialException:
+            print("nie pradzi")
+            
+    def _send(self, command):
+        self.serial.write(command)
 
 
+    def set_pos(self, pos):
+        self._send(f" {pos}")
+    
+    def get_pos(self,pos):
+        return pos
 
-class SMC100CC:
-    def __init__(self):
-        return None
+    
